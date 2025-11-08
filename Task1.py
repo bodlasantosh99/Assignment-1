e47376e6486e153b68b5314aacc8e0c609e3efef
bodlasantosh99 <123237286+bodlasantosh99@users.noreply.github.com>
2025-11-08 22:02:17 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+# Perform Addition,Subtraction,Multiplication and Division by taking tow numbers as input from User
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition :",num1+num2)
+print("Subtraction :",num1-num2)
+print("Multiplication :",num1*num2)
+print("Division :",num1/num2)
+
---
+++ Task2.py
@@ -0,0 +1,7 @@
+# Take first name and last Name inputs from user and print a message.
+
+Firstname = input("Enter your first name :")
+Lastname = input("Enter your last name :")
+
+print(f"Hello {Firstname} {Lastname}! How are you doing today?" )
+
